71d31298ea751b60be796d4b5cb26f35ceb03723
Fabrizio Cuscini <fabriziocuscini@me.com>
2026-02-23 15:56:43 +0000

    Send email for founding community

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,19 @@
+import { resolve } from 'path';
 import posthtml from '@malobre/vite-plugin-posthtml';
 import components from 'posthtml-component';
 import FullReload from 'vite-plugin-full-reload';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        terms: resolve(__dirname, 'terms.html'),
+        privacy: resolve(__dirname, 'privacy.html'),
+      },
+    },
+  },
   plugins: [
     posthtml({
       plugins: [
@@ -16,6 +26,11 @@ export default defineConfig({
         recognizeSelfClosing: true,
       },
     }),
-    FullReload(['index.html', 'src/components/**/*.html']),
+    FullReload([
+      'index.html',
+      'terms.html',
+      'privacy.html',
+      'src/components/**/*.html',
+    ]),
   ],
 });
